5e332d2f799d4a4373437e33d0f4bfdf322acd51
sangwoo <tkddn3367@gmail.com>
2023-05-03 08:46:49 +0900

    draw stations

--- index.js
+++ index.js
@@ -4,34 +4,20 @@ function calculateArrayAverage(arr) {
 }
 
 function setLocationAverage() {
-    const lats = stations.filter((station) => station.lat).map((station) => station.lat);
+    const lats = stations.map((station) => station.lat);
     LAT_AVG = calculateArrayAverage(lats);
 
-    const lngs = stations.filter((station) => station.lng).map((station) => station.lng);
+    const lngs = stations.map((station) => station.lng);
     LNG_AVG = calculateArrayAverage(lngs);
 }
 
-function getMapWidth() {
-    const {
-        0: min_lat,
-        length: length_lat,
-        [length_lat - 1]: max_lat
-    } = stations
-        .filter((station) => station.lat)
-        .map((station) => station.lat)
-        .sort();
+function getMapHeight() {
+    const { 0: min_lat, length: length_lat, [length_lat - 1]: max_lat } = stations.map((station) => station.lat).sort();
     return (max_lat - min_lat) * HEIGHT_RATIO;
 }
 
-function getMapHeight() {
-    const {
-        0: min_lng,
-        length: length_lng,
-        [length_lng - 1]: max_lng
-    } = stations
-        .filter((station) => station.lng)
-        .map((station) => station.lng)
-        .sort();
+function getMapWidth() {
+    const { 0: min_lng, length: length_lng, [length_lng - 1]: max_lng } = stations.map((station) => station.lng).sort();
     return (max_lng - min_lng) * WIDTH_RATIO;
 }
 
